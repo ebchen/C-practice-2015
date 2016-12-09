1decc84d84e04a31fb8145860064356805d6210a
ebchen <eric.chen.us@gmail.com>
2016-12-08 19:49:07 -0600

    Delete Eric_Currency.py

--- Eric_Currency.py
+++
@@ -1,8 +0,0 @@
-class Eric_Currency:
-    __init__(self,amount=0,cc="USD",clarification):
-        self.amount=amount
-        self.cc=cc
-        self.clarification=clarification
-    __str__(self):
-        return str(amount)+" "+cc
-    convert_to(self,cc2):
