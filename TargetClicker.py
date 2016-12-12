eb60514f011fb13c0a3e0ebce424cc0eb940aa4a
ebchen <eric.chen.us@gmail.com>
2016-12-11 23:41:30 -0600

    Update and rename HarambeClicker.py to TargetClicker.py

--- TargetClicker.py
+++ TargetClicker.py
@@ -7,14 +7,14 @@ def clicked():
 	global Harambes
 	global tk_Harambes
 	Harambes+=1
-	tk_Harambes.set("Times Gorilla has been clicked: " + str(Harambes))
+	tk_Harambes.set("Times Target has been clicked: " + str(Harambes))
 	PlaySound('gunsound.wav', SND_ASYNC)
 
 root = Tk()
 
 Harambes = 0
 tk_Harambes = StringVar()
-tk_Harambes.set("Times Gorilla has been clicked: " + str(Harambes))
+tk_Harambes.set("Times Target has been clicked: " + str(Harambes))
 
 img = PhotoImage(file="Harambe.gif")
 Harambe_button = Button(root, image=img, command=clicked)
